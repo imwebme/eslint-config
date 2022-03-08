e0b97dd97dc7f53f8deaf680e08d150765239a06
Hansanghyeon <999@hyeon.pro>
2022-03-08 11:31:16 +0900

    fix: eslint standard 참고제작

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-module.exports = {
-  env: {
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    camelcase: ['error', { properties: 'never' }],
-
-    // 클래스 메서드 안에서 반드시 this를 사용할 필요 없음
-    'class-methods-use-this': 'off',
-
-    // 행의 최대 길이는 120
-    'max-len': ['warn', 120],
-
-    // 문장의 마지막에 존재하는 주석 시작 전에 한 개 이상의 공백을 허용
-    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
-  },
-};
+/* eslint-disable import/no-unresolved */
+module.exports = require('./eslintrc.json');

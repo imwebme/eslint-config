907d50d8aee3a1d309f815b95f82c421f313cf85
Hansanghyeon <hyeon@imweb.me>
2022-03-11 09:35:51 +0900

    chore: module test 05

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-module.exports = {
-  env: {
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    "camelcase": ["error", { "properties": "never" }],
-    "class-methods-use-this": "off",
-    "max-len": ["warn", 120],
-    "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
-  }
-}
+/* eslint-disable import/no-unresolved */
+module.exports = require('./eslintrc.json');
